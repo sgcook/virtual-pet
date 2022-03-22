f20a4c8df71e218f8f14824046d713693a069580
Sara Cook <saracook087@gmail.com>
2022-03-22 15:49:10 +0000

    added hunger and fitness properties

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -14,12 +14,38 @@ describe("constructor", () => {
         const pet = new Pet("Fido");
         expect(pet.age).toEqual(0);
     });
-    
+
     it("increments the age by 1", () => {
         const pet = new Pet("Fido");
 
         pet.growUp();
 
         expect(pet.age).toEqual(1);
+    })
+
+    it("has an initial hunger of 0", () => {
+        const pet = new Pet("Fido");
+        expect(pet.hunger).toEqual(0);
+    });
+
+    it("increments hunger by 5", () => {
+        const pet = new Pet("Fido");
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+
+    it("has an initial fitness of 0", () => {
+        const pet = new Pet("Fido");
+        expect(pet.fitness).toEqual(0);
+    });
+
+    it("decreases fitness by 3", () => {
+        const pet = new Pet("Fido");
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(-3);
     })
 });
